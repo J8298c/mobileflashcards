1952bef4b33a705dfcaf3a3a7b5bc4d2a2bad9cd
Julio Mojica <juliocmojica@gmail.com>
2017-12-26 21:30:27 -0500

    refactoring and cleaning up

--- App.js
+++ App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import HomePage from './components/HomePage';
+import { View, Text } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        <HomePage />
-      </View>
-    );
-  }
-}
-
+const App = () => (
+  <View>
+    <Text>Hello World</Text>
+  </View>
+)
+export default App;
