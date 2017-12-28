617e26f3140ecf1418a5e47f7e3674a12bc6acc1
Julio Mojica <juliocmojica@gmail.com>
2017-12-28 18:04:36 -0500

    Router & Redux hooked to app

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import Deck from './components/Deck';
+import RouterComponent from './RouterComponent';
 
 const App = () => (
   <View style={styles.containerStyle}>
-    <Deck />
+    <RouterComponent />
   </View>
 );
 
---
+++ RouterComponent.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Router, Scene, Stack } from 'react-native-router-flux';
+import appReducer from './reducer/index';
+import Deck from './components/Deck';
+
+const store = createStore(appReducer, {}, applyMiddleware(thunk));
+
+const RouterComponent = () => {
+  return (
+    <Provider store={store}>
+        <Router>
+            <Stack>
+              <Scene title='Decks' key='deck' component={Deck} />
+            </Stack>
+        </Router>
+    </Provider>
+  )
+}
+export default RouterComponent;
