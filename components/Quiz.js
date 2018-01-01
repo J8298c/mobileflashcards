a2fe375c9c38c3103544f80eedf5a9cee9dc3532
Julio Mojica <juliocmojica@gmail.com>
2018-01-01 16:41:35 -0500

    styling quiz adding score container to bottom of quiz

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,39 +1,72 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import { connect } from 'react-redux';
+import Button from './Button';
 import { gettingADeck } from '../actions/index';
 
-
 class Quiz extends Component {
+	state = { score: 0}
 	componentDidMount(props) {
-		//fetch a deck 
 		this.props.gettingADeck(this.props.id);
 	}
+
+	onQuestionAnswer(answer) {
+		let newScore = this.state.score;
+		if(answer) { newScore++ }
+		console.log(newScore);
+		this.setState({ score: newScore});
+	}
+
 	render(props) {
 		return (
-			<View>
+			<ScrollView contentContainerStyle={styles.quizContainer}>
 				{
-					this.props.deck ?
+					this.props.deck.questions ?
 					this.props.deck.questions.map(question => (
-						<View key={question.question}>
-							<Text>
-								{question.question}
-							</Text>
+						<View key={question.question} style={styles.viewContaier}>
+							<Text style={styles.textContainer}>{question.question}</Text>
+							<Text style={styles.textContainer}>{question.answer}</Text>
+							<Button buttonText='Correct' onPress={() => { this.onQuestionAnswer(true)}} />
+							<Button buttonText='InCorrect' onPress={() => { this.onQuestionAnswer(false)}} />
 						</View>
 					))
 					: null
 				}
-			</View>
+				<Text style={styles.scoreContainer}>
+					Current Score: { this.state.score }
+				</Text>
+			</ScrollView>
 		)
 	}
 }
 
+const styles = {
+	quizContainer: {
+		flex: 1,
+		paddingVertical: 20
+	},
+	viewContaier: {
+		marginTop: 20,
+		marginBottom: 55
+	},
+	textContainer: {
+		alignItems: 'center',
+		marginLeft: 50,
+		marginRight: 50
+	},
+	scoreContainer: {
+		fontSize: 20,
+		color: 'blue',
+		alignItems: 'center'
+	}
+}
+
 function mapStateToProps(state) {
-	console.log(state);
+	console.log(state)
 	const { deck } = state;
 	return {
 		deck
 	}
 }
 
-export default connect(mapStateToProps, { gettingADeck })(Quiz);
+export default connect(mapStateToProps, {gettingADeck})(Quiz);
