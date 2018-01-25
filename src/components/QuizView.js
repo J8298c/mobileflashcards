e5b43907de2f017d4110088e3fc2a832efa1fd6e
Julio Mojica <juliocmojica@gmail.com>
2018-01-24 20:23:41 -0500

    quiz completed

--- src/components/QuizView.js
+++ src/components/QuizView.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
 import { Text, View, AsyncStorage } from 'react-native';
+import AppButton from './AppButton';
 
 class QuizView extends Component {
-    state ={}
+    state = {
+        score: 0,
+        message: ''
+    }
     componentDidMount(props) {
         AsyncStorage.getItem('Decks')
         .then(response => {
@@ -15,16 +19,46 @@ class QuizView extends Component {
         .catch(error => { console.log(error)})
     }
 
+    incrementScore() {
+      const {score} = this.state;
+      let newScore = score + 1;
+      this.setState({ score: newScore, message: '' })
+    }
+
+
     render(props) {
         console.log(this.state)
         return (
             <View>
-                <Text>
-                    QuizView
-                </Text>
+                {
+                    this.state.deck ? 
+                    this.state.deck.questions.map(quiz => (
+                        <View key={quiz.question}>
+                            <View>
+                            <Text>{quiz.question}</Text>
+                            </View>
+                            <View>
+                                <Text>{quiz.answer}</Text>
+                            </View>
+                            <AppButton buttonText='Correct' onPress={() => { this.incrementScore()}} />
+                            <AppButton buttonText='Not Correct' onPress={() => { this.setState({message: 'Wrong answer'}) }} />
+                            <View>
+                                <Text>{this.state.score}</Text>
+                                <Text>{this.state.message}</Text>
+                            </View>
+                        </View>
+                    ))
+                    :null
+                }
             </View>
         )
     }
 }
 
 export default QuizView;
+
+const style = {
+    errorBtn: {
+        backgroundColor: 'red'
+    }
+}
