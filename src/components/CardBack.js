06a06bfd8cc2001794366eae474206d8606f4504
Julio Mojica <juliocmojica@gmail.com>
2018-01-25 21:34:59 -0500

    fixing error

--- src/components/CardBack.js
+++ src/components/CardBack.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import AppButton from './AppButton';
-import { containerStyle } from './NotificationHellper';
+
 
 const CardBack = (props) => (
     <View style={styles.containerStyle}>
